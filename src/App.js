54b557d68ee72c8387f5fd1fff9b3627ab064151
Mattlotus <mattlotus91@gmail.com>
2023-08-06 21:03:32 -0400

    bit coin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Currencies from './pages/Currencies'
+import Main from './pages/Main'
+import Prices from './pages/Price'
+import Nav from './components/Nav'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const App = () => {
+    return (
+      //  we will use the Route component to specify each route
+      // path ... if i go to "blank" site.com/path .... browserRouter automically looks into pages folder (?) or that logic comes from import statement
+      // element calls the page you want
+      // :symbol -> imports name of symbol here.. "URL param" variable in URL
+      <div className='App'>
+          <Nav />
+        <Routes>
+          {/* / because its the root page. landing page */}
+          <Route path="/" element={<Main />}/> 
+          <Route path="/currencies" element={<Currencies />}/>
+          <Route path="/price/:symbol" element={<Prices />}/>
+        </Routes>
+      </div>
+    )
+  }
+  
+  export default App
